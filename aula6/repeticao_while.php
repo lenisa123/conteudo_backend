d872cd6153ae37a1cc78d0de85de24941c4a26c1
Lenisa Sousa <lenisa.sousa@alunos.ifsuldeminas.edu.br>
2023-03-08 10:37:12 -0300

    Repetição for

---
+++ aula6/repeticao_while.php
@@ -0,0 +1,19 @@
+<h2>Estrutura de repetição While</h2>
+<hr>
+<p>
+    Utilizada para repetir um conjunto de instruções.
+</p>
+<strong>Exemplo</strong>
+<pre>
+    wihle(condição){
+        //comandos a serem repetidos.
+    }
+</pre>
+
+<?php
+  $contador = 0;
+  while(10){
+        echo"Olá pessoal $contador<br>";
+        $contador++;
+    }
+?>
