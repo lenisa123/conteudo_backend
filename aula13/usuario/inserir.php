31c63e6f5ef0378acbf42f25ef148861fd3bf137
lenisa <lenisa.sousa@alunos.ifsuldeminas.edu.br>
2023-05-10 10:47:08 -0300

    aula15

--- aula13/usuario/inserir.php
+++ aula13/usuario/inserir.php
@@ -3,6 +3,11 @@
    //importa o arquivo de conexão
    require_once "../conexao.php";
 
+   //verifica se o nome, login e senha foram enviados 
+   //do formulario de cadastro
+   if(isset($_POST['nome'])&&isset($_POST['login'])&&
+      isset($_POST['senha'])){
+
    $nome = $_POST['nome'];
    $login = $_POST['login'];
    $senha = password_hash($_POST['senha'], PASSWORD_BCRYPT);
@@ -18,9 +23,9 @@
 
    //executa o comando
    $comando->execute();
-
+   }
    //volta para o formulário
-   header("Location: ../form_usuario.php");
+   header("Location: ../usuarios.php");
 
    
 
