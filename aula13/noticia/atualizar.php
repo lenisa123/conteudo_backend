39223bd470b5bfba7db2793b45795f5e6819b91c
lenisa <lenisa.sousa@alunos.ifsuldeminas.edu.br>
2023-05-23 08:49:24 -0300

    crud noticia

---
+++ aula13/noticia/atualizar.php
@@ -0,0 +1,33 @@
+<?php
+   
+   //importa o arquivo de conexão
+   require_once "../banco/conexao.php";
+
+   $idnoticia = $_POST['idnoticia'];
+   $titulo = $_POST['titulo'];
+   $materia = $_POST['materia'];
+   $categoria = $_POST['categoria'];
+
+   //cria uma variável com um comando SQL
+   $SQL = "UPDATE `noticia` SET `titulo`= ?, `materia`= ?, `categoria`= ? WHERE  `idnoticia`= ? ;";
+ 
+   //prepara o comando para ser executado no mysql
+   $comando = $conexao->prepare($SQL);
+
+   //faz a vinculação dos parâmetros ?, ?, ?
+   $comando->bind_param("sssi", $titulo, $materia, $categoria, $id);
+
+   //executa o comando
+   $comando->execute();
+
+   //volta para o formulário
+   header("Location: index.php");
+
+   
+
+
+
+
+
+
+
--- aula13/usuario/formulario.php
+++ aula13/usuario/formulario.php
@@ -1,7 +1,6 @@
 <?php
     require_once "consultar_por_id.php";
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -14,22 +13,18 @@
     <h1>Cadastro de usuário</h1>
     <hr>
 
-    <form action="<?php echo isset($usuario) ? 
-    "atualizar.php" :
-    "inserir.php"; ?>" 
-    
-    method="post" enctype="multipart/form-data">
-    
-        <input type="hidden" name="idusuario" value="<?php echo $usuario->idusuario ?? "";?>"><br>
+    <form action="<?php echo isset($usuario) ? "atualizar.php" : "inserir.php";?>" method="post" enctype="multipart/form-data">
+
+        <input type="hidden" name="idusuario" value="<?php echo $usuario->idusuario ?? "" ;?>"><br>
 
         <label>Nome</label><br>
-        <input type="text" name="nome" value="<?php echo $usuario->nome ?? "";?>"><br>
+        <input type="text" name="nome" value="<?php echo $usuario->nome ?? "" ;?>"><br>
 
         <label>Login</label><br>
-        <input type="text" name="login" value="<?php echo $usuario->login ?? "";?>"><br>
+        <input type="text" name="login" value="<?php echo $usuario->login ?? "" ;?>"><br>
 
         <label>Senha</label><br>
-        <input type="password" name="senha" value="<?php echo $usuario->senha ?? "";?>"><br>
+        <input type="password" name="senha" value="<?php echo $usuario->senha ?? "" ;?>"><br>
 
         <label>Foto</label><br>
         <input type="file" name="foto"><br><br>
