9bbda4675886426620a69a47a5ad1c1aa553ff2d
lenisa <lenisa.sousa@alunos.ifsuldeminas.edu.br>
2023-05-24 10:38:26 -0300

    template

--- aula13/usuario/formulario.php
+++ aula13/usuario/formulario.php
@@ -1,15 +1,10 @@
 <?php
     require_once "consultar_por_id.php";
+    require_once "../template/cabecalho.php";
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
+
+<div class="container">
+
     <h1>Cadastro de usuário</h1>
     <hr>
 
@@ -33,7 +28,6 @@
         
     </form>
 
+</div>
 
-    
-</body>
-</html>
+<?php require_once "../template/rodape.php" ;?>
